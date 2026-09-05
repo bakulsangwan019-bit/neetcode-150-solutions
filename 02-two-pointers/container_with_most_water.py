8cc336908f1a0441b7342a40cc4b6b6713d9b773
Bakul Sangwan <236530718+bakulsangwan019-bit@users.noreply.github.com>
2026-09-05 12:00:40 +0530

    Update Container With Most Water with both approaches

--- 02-two-pointers/container_with_most_water.py
+++ 02-two-pointers/container_with_most_water.py
@@ -1,3 +1,31 @@
+# Container With Most Water
+
+
+# Approach 1 - Brute Force
+
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+largest = 0
+
+for i in range(len(height)):
+    for j in range(i+1 , len(height)):
+        width = j - i
+        height_che = min(height[i] , height[j])
+
+        wsable_height = width * height_che
+
+        if wsable_height > largest:
+            largest = wsable_height
+
+print(largest)
+
+
+# Time Complexity: O(n^2)
+# Auxiliary Space: O(1)
+
+
+
+# Approach 2 - Two Pointers
+
 height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 largest = 0
 
@@ -5,27 +33,23 @@ left = 0
 right = len(height) - 1
 
 while left < right:
-    
+
     width = right - left
     u_height = min(height[left] , height[right])
 
+    height_ch = u_height * width
 
-    if u_height == height[left]:
-        height_ch = u_height * width
-
+    if u_height == height[left]: # or we directly use if height[left] < height[right]
         left += 1
 
     if u_height == height[right]:
-        height_ch = u_height * width
-
         right -= 1
-    
-    
-    
-    
+
     if height_ch > largest:
         largest = height_ch
 
 print(largest)
 
-    
+
+# Time Complexity: O(n)
+# Auxiliary Space: O(1)
