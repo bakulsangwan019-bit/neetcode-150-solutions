02a3fc9ed205823f7d2eecec910a3e7a583bad1f
Bakul Sangwan <236530718+bakulsangwan019-bit@users.noreply.github.com>
2026-09-05 11:53:04 +0530

    Add brute force approach to Container With Most Water

--- 02-two-pointers/container_with_most_water.py
+++ 02-two-pointers/container_with_most_water.py
@@ -1,64 +1,31 @@
-# Container With Most Water
-
-
-# Approach 1 - Brute Force
-# Check every possible pair
-
-def max_area_brute(height):
-
-    largest_area = 0
-
-    for i in range(len(height)):
-        for j in range(i + 1, len(height)):
-
-            width = j - i
-            usable_height = min(height[i], height[j])
-
-            area = width * usable_height
-
-            if area > largest_area:
-                largest_area = area
-
-    return largest_area
-
-
-# Time Complexity: O(n^2)
-# Auxiliary Space: O(1)
-
-
-
-# Approach 2 - Two Pointers
-
-def max_area(height):
-
-    left = 0
-    right = len(height) - 1
-
-    largest_area = 0
-
-    while left < right:
-
-        width = right - left
-        usable_height = min(height[left], height[right])
-
-        area = width * usable_height
-
-        if area > largest_area:
-            largest_area = area
-
-        if height[left] < height[right]:
-            left += 1
-
-        else:
-            right -= 1
-
-    return largest_area
-
-
 height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+largest = 0
 
-print(max_area(height))
+left = 0
+right = len(height) - 1
+
+while left < right:
+    
+    width = right - left
+    u_height = min(height[left] , height[right])
 
 
-# Time Complexity: O(n)
-# Auxiliary Space: O(1)
+    if u_height == height[left]:
+        height_ch = u_height * width
+
+        left += 1
+
+    if u_height == height[right]:
+        height_ch = u_height * width
+
+        right -= 1
+    
+    
+    
+    
+    if height_ch > largest:
+        largest = height_ch
+
+print(largest)
+
+    
